bd4727d0741fd86d14be25ac44498117b19ed1be
Renan Lopes <r.maruffa@gmail.com>
2022-07-27 09:16:56 -0300

    Atividade a009 concluida

---
+++ exercicio1/script.js
@@ -0,0 +1,19 @@
+// let nomeDoUsuario = prompt("Me diga seu nome")
+// let comidaFav = prompt("Agora me fale suas 3 comidas preferidas")
+
+// console.log(`Então ${nomeDoUsuario} estas são suas comidas preferidas:
+// - ${comidaFav.split(" ")}`)
+
+// --------------------------
+
+let nomeDoUsuario = prompt("Me diga seu nome")
+let comidaFav1 = prompt("Agora me fale uma comidas de suas comidas preferidas...")
+let comidaFav2 = prompt("Legal, me fala mais uma")
+let comidaFav3 = prompt("Show, mais uma, entao?")
+
+console.log(`Então ${nomeDoUsuario} suas comidas prefiletas sao 
+${comidaFav1} 
+${comidaFav2} e 
+${comidaFav3}`);
+
+
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,11 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+
+const zerandoCaract = minhaString.trim()
+
+console.log(`Quantidade de caracteres com excesso de espaço ${minhaString.length}
+e a quantidade de caracteres depois da remoção dos espaços é ${zerandoCaract.length}`)
+
+const newString = minhaString.replaceAll ("________", "sticioso")
+
+console.log(newString);
+
